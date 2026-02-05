507b690617d1b466b8b65903d0cb7c36b78e17b9
bytelisa <elisa.marzioli2002@gmail.com>
2026-02-05 12:40:09 +0100

    first draft worker main and service

---
+++ cmd/worker/worker_main.py
@@ -0,0 +1,35 @@
+
+# the main turns on the server and manages the connection
+
+import logging
+import sys
+import os
+from concurrent import futures
+import grpc
+
+sys.path.append(os.getcwd())
+
+from api.proto.worker.v1 import worker_pb2_grpc
+from services.worker.worker_service import WorkerService
+
+def serve():
+    print("Worker service starting...")
+
+    # Create a gRPC sever
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    # Register WorkerService on the server
+    worker_pb2_grpc.add_WorkerServiceServicer_to_server(WorkerService(), server)
+
+    # Listen on port 50051
+    port = "50051"
+    server.add_insecure_port(f'[::]:{port}')
+    print(f"Worker server listening on port {port}...")
+
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == "__main__":
+    logging.basicConfig()
+    serve()
